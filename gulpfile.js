03c21ff2cb0f308fd7a3d0aaaa6793639ae0b195
unknown <mohamed.omezzine@gmail.com>
2015-05-06 15:35:54 +0200

    fix livereload

--- gulpfile.js
+++ gulpfile.js
@@ -23,18 +23,18 @@ var path = {
 	},
 
 	images: {
-		src: './app/images/**',
+		src: './app/images/**/*.*',
 		dest: './dist/images/'
 	},
 
 	fonts: {
-		src: './app/fonts/**',
+		src: './app/fonts/**/*.*',
 		dest: './dist/fonts/'
 	}
 
 };
 
-// Setup The Server
+// Setup The Dev Server
 gulp.task('connect', function() {
   $.connect.server({
     root: './app',
@@ -42,14 +42,26 @@ gulp.task('connect', function() {
   });
 });
 
+// Run prod version
+gulp.task('connectProd', function() {
+  $.connect.server({
+    root: './dist',
+    livereload: false
+  });
+});
+
+// Reload the server
+gulp.task('reload', function(){
+	return	gulp.src(['./app/js/app.js', 'app/js/**/*.js', './app/templates/**/*.html', './app/styles/css/**/*.css'])
+				.pipe($.connect.reload());
+})
+
 // Sass To CSS
 gulp.task('styles', function(){
-	console.log('test');
 	return	gulp.src(path.sass)
 			.pipe($.sass({onError: function(e) { console.log(e); } }))
 			.pipe($.autoprefixer("last 2 versions", "> 1%", "ie 8"))
-			.pipe(gulp.dest(path.css.directory))
-			.pipe($.connect.reload());
+			.pipe(gulp.dest(path.css.directory));
 });
 
 // JSHint task
@@ -73,7 +85,7 @@ gulp.task('usemin', function () {
 
 // Clean The Dist folter
 gulp.task('clean-dist', function(cb){
-	$.rimraf('./dist', cb);
+	$.rimraf('./dist/**/*.*', cb);
 });
 
 // Copy templates to dist
@@ -85,23 +97,25 @@ gulp.task('copyTpls', function(){
 
 // Copy images to dist
 gulp.task('copyImgs', function(){
-	return gulp.src(path.images.src, { read: false })
+	return gulp.src(path.images.src)
 	    .pipe(gulp.dest(path.images.dest));
 });
 
 // Copy fonts to dist
 gulp.task('copyFonts', function(){
-	return gulp.src(path.fonts.src, { read: false })
+	return gulp.src(path.fonts.src)
 	    .pipe(gulp.dest(path.fonts.dest));
 });
 
 // Setup The Watchers
 gulp.task('watch', function(){
 	// SASS Watcher
-	gulp.watch(path.sass, ['styles']);
+	gulp.watch(path.sass, ['styles', 'reload']);
 	// JavaScript Watcher
-	gulp.watch(path.scripts, ['lint']);
+	gulp.watch(path.scripts, ['lint', 'reload']);
+	// Templates Watcher
+	gulp.watch(path.templates.src, ['reload']);
 });
 
 gulp.task('default', ['connect', 'watch']);
-gulp.task('dist', ['clean-dist', 'usemin', 'copyTpls', 'copyImgs', 'copyFonts']);
+gulp.task('dist', ['clean-dist', 'usemin', 'copyTpls', 'copyImgs', 'copyFonts', 'connectProd']);
